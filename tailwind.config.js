1ef61cc776ecf6c4dce249eb7919a663d0cc76ff
ITD16 <it.support@d16pro.com>
2025-07-12 21:37:21 +0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}',
-    './components/**/*.{astro,html,js,jsx,ts,tsx}',
-    './layouts/**/*.{astro,html,js,jsx,ts,tsx}',
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', // đảm bảo đúng đường dẫn của bạn
   ],
   safelist: [
     'text-[150px]',
     'text-[200px]',
     'text-[240px]',
-    '!text-[150px]',
-    '!text-[200px]',
-    '!text-[240px]',
   ],
   theme: {
     extend: {},
