9792a01717a42eee221c7af5d9c816bbc31e552b
ITD16 <it.support@d16pro.com>
2025-07-12 21:39:48 +0700

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,7 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind({ config: './tailwind.config.js' })],
   adapter: netlify()
 });
-
